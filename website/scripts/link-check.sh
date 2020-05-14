9a60923d0216279a08edf235b817ace1a7f7fa1a
Alvin Huang <17609145+alvin-huang@users.noreply.github.com>
2020-05-13 23:17:40 -0400

    add link check back

---
+++ website/scripts/link-check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -xe
+
+# Install netlify-cli
+npm install netlify-cli
+
+# set path to grab the netlify binary
+export PATH=$PATH:$(npm bin)
+
+# Deploy site to netlify
+# Assumes NETLIFY_SITE_ID and NETLIFY_AUTH_TOKEN env variables are set
+output=$(netlify deploy --dir=./website/out --json)
+
+# Grab deploy URL
+url=$(echo "$output" | jq --raw-output '.deploy_url')
+
+# Checks broken links
+wget \
+  --delete-after \
+  --level inf \
+  --no-verbose \
+  --recursive \
+  --no-directories \
+  --no-host-directories \
+  --page-requisites \
+  --spider \
+  $url
